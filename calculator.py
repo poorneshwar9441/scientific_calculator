838bc6d6398e5e9fe00dfbc7f52d4a17b36b55f1
poorneshwar9441 <poorneshchaganti@gmail.com>
2023-10-30 20:17:44 +0530

    init

---
+++ calculator.py
@@ -0,0 +1,80 @@
+import math
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero"
+    return x / y
+
+def square_root(x):
+    if x < 0:
+        return "Error: Square root of a negative number"
+    return math.sqrt(x)
+
+def sin(x):
+    return math.sin(math.radians(x))
+
+def cos(x):
+    return math.cos(math.radians(x))
+
+def tan(x):
+    return math.tan(math.radians(x))
+
+def main():
+    while True:
+        print("Scientific Calculator Menu:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Square Root")
+        print("6. Sine")
+        print("7. Cosine")
+        print("8. Tangent")
+        print("9. Exit")
+
+        choice = input("Enter your choice (1/2/3/4/5/6/7/8/9): ")
+
+        if choice == '9':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+
+        elif choice == '5':
+            num1 = float(input("Enter a number: "))
+
+        elif choice in ('6', '7', '8'):
+            angle = float(input("Enter an angle in degrees: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+        elif choice == '5':
+            result = square_root(num1)
+        elif choice == '6':
+            result = sin(angle)
+        elif choice == '7':
+            result = cos(angle)
+        elif choice == '8':
+            result = tan(angle)
+        
+        print("Result:", result)
+
+if __name__ == "__main__":
+    main()
---
+++ test_calculator.py
@@ -0,0 +1,92 @@
+import unittest
+import logging
+from unittest.mock import patch
+from io import StringIO
+from calculator import main
+
+# Configure the logging module
+logging.basicConfig(filename='logs/test_results.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+class TestScientificCalculator(unittest.TestCase):
+
+    def log_result(self, test_name, result):
+        if result:
+            logging.info(f"{test_name}: Passed")
+        else:
+            logging.error(f"{test_name}: Failed")
+
+    @patch('builtins.input', side_effect=['1', '5', '3', '9'])
+    def test_addition(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 8.0" in output
+            self.log_result('test_addition', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['2', '5', '3', '9'])
+    def test_subtraction(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 2.0" in output
+            self.log_result('test_subtraction', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['3', '5', '3', '9'])
+    def test_multiplication(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 15.0" in output
+            self.log_result('test_multiplication', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['4', '6', '2', '9'])
+    def test_division(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 3.0" in output
+            self.log_result('test_division', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['5', '25', '9'])
+    def test_square_root(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 5.0" in output
+            self.log_result('test_square_root', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['6', '30', '9'])
+    def test_sin(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 0.49999999999999994" in output
+            self.log_result('test_sin', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['7', '60', '9'])
+    def test_cos(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 0.5000000000000001" in output
+            self.log_result('test_cos', result)
+            self.assertTrue(result)
+
+    @patch('builtins.input', side_effect=['8', '45', '9'])
+    def test_tan(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            main()
+            output = mock_output.getvalue()
+            result = "Result: 0.9999999999999999" in output
+            self.log_result('test_tan', result)
+            self.assertTrue(result)
+
+if __name__ == '__main__':
+    unittest.main()
+
